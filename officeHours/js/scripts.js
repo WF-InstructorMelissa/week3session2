fc2079df601563883c5aef30d200b8af68e2f168
Melissa Longenberger <mlongenberger@codingdojo.com>
2021-07-21 18:14:11 -0400

    adding base files

---
+++ officeHours/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $.get("https://looney-toons-api.herokuapp.com/api/characters", function(res) {
+        console.log(res.data)
+        var html_str = "";
+        for(var i = 0; i < res.data.length; i++) {
+            html_str += "<section class='toons'>";
+            html_str += "<h3>" + res.data[i].name + "</h3>";
+            html_str += "<img src='" + res.data[i].img + "' alt='" + res.data[i].name + "' />"
+            html_str += "</section>";
+        }
+        $(".main").html(html_str);
+    }, "json");
+})
+
+function myFunction() {
+    alert("You clicked a Toon!")
+}
